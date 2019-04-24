521ed41ca1f80bd8a946e21c7f2455fb8f4493fd
Jonatan Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2019-04-24 14:51:00 +0200

    Added Constraints

--- app/src/main/java/com/jonatan_vahlberg/gitasignment/MainActivity.java
+++ app/src/main/java/com/jonatan_vahlberg/gitasignment/MainActivity.java
@@ -2,6 +2,10 @@ package com.jonatan_vahlberg.gitasignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button topBtn = findViewById(R.id.topBtn);
+        Button bottomBtn = findViewById(R.id.bottomBtn);
+
+        topBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked the Top button",Toast.LENGTH_SHORT).show();
+                Log.i("Info","The User Clicked the top button");
+            }
+        });
+
+        bottomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked the bottom button",Toast.LENGTH_LONG).show();
+                Log.i("Info","The User Clicked the Bottom button");
+            }
+        });
     }
 }
